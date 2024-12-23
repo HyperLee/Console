9bff836900625d613f4d2f9a0d2f4aae72956925
Peter.Chiu <tidub76217@hotmail.com>
2024-12-23 22:00:33 +0800

    2471. Minimum Number of Operations to Sort a Binary Tree by Level
    練習題目

--- Console/ConsoleApp1/Program.cs
+++ Console/ConsoleApp1/Program.cs
@@ -2,48 +2,85 @@
 // Console.WriteLine("Hello, World!");
 
 // main function
-int[] input = { 4, 3, 2, 1, 0 };
-Console.WriteLine("method1: " + MaxChunksToSorted(input));
-Console.WriteLine("method2: " + MaxChunksToSorted2(input));
 
-static int MaxChunksToSorted(int[] arr)
+using System.Security.AccessControl;
+
+TreeNode root = new TreeNode(1);
+
+root.left = new TreeNode(3);
+root.right = new TreeNode(2);
+
+root.left. left = new TreeNode(7);
+root.left.right = new TreeNode(6);
+
+root.right.left = new TreeNode(5);
+root.right.right = new TreeNode(4);
+
+Console.WriteLine("res: " + MinimumOperations(root));
+
+static int MinimumOperations(TreeNode root)
 {
-    int m = 0;
     int res = 0;
+    Queue<TreeNode> queue = new Queue<TreeNode>();
+    queue.Enqueue(root);
 
-    for(int i = 0; i < arr.Length; i++)
+    while(queue.Count > 0)
     {
-        m = Math.Max(m, arr[i]);
-        if(m == i)
+        int size = queue.Count;
+        int[] arr = new int[size];
+        int[] sorted = new int[size];
+        for(int i = 0; i < size; i++)
         {
-            res++;
+            TreeNode node = queue.Dequeue();
+            arr[i] = node.val;
+            sorted[i] = node.val;
+            if(node.left != null)
+            {
+                queue.Enqueue(node.left);
+            }
+            if(node.right != null)
+            {
+                queue.Enqueue(node.right);
+            }
+        }
+        Array.Sort(sorted);
+        IDictionary<int, int> targetIndices = new Dictionary<int, int>();
+        for(int i = 0; i < size; i++)
+        {
+            targetIndices.Add(sorted[i], i);
+        }
+
+        for(int i = 0; i < size; i++)
+        {
+            while(arr[i] != sorted[i])
+            {
+                int targetIndex = targetIndices[arr[i]];
+                Swap(arr, i, targetIndex);
+                res++;
+            }
         }
     }
 
     return res;
 }
 
-static int MaxChunksToSorted2(int[] arr)
+
+static void Swap(int[] arr, int index1, int index2)
 {
-    int n = arr.Length;
-    int res = 0;
-
-    for(int i = 0, j = 0, min = n, max = -1; i < n; i++)
-    {
-        min = Math.Min(min, arr[i]);
-        max = Math.Max(max, arr[i]);
-
-        if(j == min && i == max)
-        {
-            res++;
-            j = i + 1;
-            min = 0;
-            max = -1;
-        }
-    }
-
-    return res;
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp; 
 }
 
-
-
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
--- Console/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ Console/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e1a28cc58dec7a233caaa90cc663a651f587df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9ef1240da1be34f4677ed64c414b8e98c23dca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
