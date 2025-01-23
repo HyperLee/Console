4966c0e73d70dabd20e72476d08f6e22ae1f78bc
Peter.Chiu <tidub76217@hotmail.com>
2025-01-23 21:36:37 +0800

    加入Docker
    加入Docker

--- Console/ConsoleApp1/Program.cs
+++ Console/ConsoleApp1/Program.cs
@@ -8,72 +8,17 @@ namespace ConsoleApp1
  
         static void Main(string[] args)
         {   
-            int[][] prerequisites = new int[][]
+            // 99乘法表
+            for (int i = 1; i <= 9; i++)
             {
-                 new int[]{ 1, 0 }
-            };
-
-            int numCourses = 2;
-
-            Console.WriteLine("res: " + CanFinish(numCourses, prerequisites));    
-        }
-
-        static IList<int>[] graph;
-        static int[] states;
-
-        public static bool CanFinish(int numCourses, int[][] prerequisites)
-        {
-            graph = new List<int>[numCourses];
-            states = new int[numCourses];
-            Array.Fill(states, 0);
-
-            for(int i = 0; i < numCourses; i++)
-            {
-                graph[i] = new List<int>();
-            }
-
-            foreach(int[] prerequisite in prerequisites)
-            {
-                graph[prerequisite[1]].Add(prerequisite[0]);
-            }
-
-            for(int i = 0; i < numCourses; i++)
-            {
-                bool valid = DFS(i);
-                if(!valid)
+                for (int j = 1; j <= 9; j++)
                 {
-                    return false;
+                    Console.Write($"{i} * {j} = {i * j}\t");
                 }
+                Console.WriteLine();
             }
-
-            return true;
         }
 
-        public static bool DFS(int curr)
-        {
-            if(states[curr] == 1)
-            {
-                return false;
-            }
-
-            if(states[curr] == 2)
-            {
-                return true;
-            }
-
-            states[curr] = 1;
-            IList<int> adj = graph[curr];
-            foreach(int next in adj)
-            {
-                bool valid = DFS(next);
-                if(!valid)
-                {
-                    return false;
-                }
-            }
-            states[curr] = 2;
-            return true;
-        }
 
     }
 }
