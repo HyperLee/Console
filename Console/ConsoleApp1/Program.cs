62f43981994272f1cae0387dceb62acae20dace6
Peter.Chiu <tidub76217@hotmail.com>
2025-01-24 14:53:18 +0800

    322. Coin Change
    DP 動態規劃

--- Console/ConsoleApp1/Program.cs
+++ Console/ConsoleApp1/Program.cs
@@ -8,71 +8,29 @@ namespace ConsoleApp1
  
         static void Main(string[] args)
         {   
-            int[][] prerequisites = new int[][]
-            {
-                 new int[]{ 1, 0 }
-            };
-
-            int numCourses = 2;
-
-            Console.WriteLine("res: " + CanFinish(numCourses, prerequisites));    
+            int[] coins = new int[] {1, 2, 5};
+            int amount = 11;
+            Console.WriteLine("res: " + CoinChange(coins, amount));
         }
 
-        static IList<int>[] graph;
-        static int[] states;
-
-        public static bool CanFinish(int numCourses, int[][] prerequisites)
+        public static int CoinChange(int[] coins, int amount)
         {
-            graph = new List<int>[numCourses];
-            states = new int[numCourses];
-            Array.Fill(states, 0);
-
-            for(int i = 0; i < numCourses; i++)
+            int max = amount + 1;
+            int[] dp = new int[amount + 1];
+            Array.Fill(dp, max);
+            dp[0] = 0;
+            for(int i = 1; i <= amount; i++)
             {
-                graph[i] = new List<int>();
-            }
-
-            foreach(int[] prerequisite in prerequisites)
-            {
-                graph[prerequisite[1]].Add(prerequisite[0]);
-            }
-
-            for(int i = 0; i < numCourses; i++)
-            {
-                bool valid = DFS(i);
-                if(!valid)
+                for(int j = 0; j < coins.Length; j++)
                 {
-                    return false;
+                    if(coins[j] <= i)
+                    {
+                        dp[i] = Math.Min(dp[i], dp[i - coins[j]] + 1);
+                    }
                 }
             }
 
-            return true;
-        }
-
-        public static bool DFS(int curr)
-        {
-            if(states[curr] == 1)
-            {
-                return false;
-            }
-
-            if(states[curr] == 2)
-            {
-                return true;
-            }
-
-            states[curr] = 1;
-            IList<int> adj = graph[curr];
-            foreach(int next in adj)
-            {
-                bool valid = DFS(next);
-                if(!valid)
-                {
-                    return false;
-                }
-            }
-            states[curr] = 2;
-            return true;
+            return dp[amount] > amount ? -1 : dp[amount]; 
         }
 
     }
