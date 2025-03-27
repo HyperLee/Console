7ef15facdd0c21c3c9a93a05492ab115dc94b082
Peter.Chiu <tidub76217@hotmail.com>
2025-03-27 15:39:14 +0800

    觀察 GitHub Actions 的運行
    觀察 GitHub Actions 的運行

--- Console/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ Console/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aff93ac297a91f9263cb07956406d831795caba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c757a7fe77dc73c54518deb48877213ad2d31e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
