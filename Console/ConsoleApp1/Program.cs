8e244c61b818de9aefc7a4df6104e2884c907017
Peter.Chiu <tidub76217@hotmail.com>
2025-03-27 15:05:54 +0800

    測試用程式碼
    測試用程式碼

--- Console/ConsoleApp1/Program.cs
+++ Console/ConsoleApp1/Program.cs
@@ -5,44 +5,62 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        /// <summary>
+        ///  LeetCode 1. Two Sum
+        ///  https://leetcode.com/problems/two-sum/
+        ///  1. 两数之和
+        /// https://leetcode.cn/problems/two-sum/description/
+        /// 
+        ///  Given nums = [2, 7, 11, 15], target = 9,
+        ///  Because nums[0] + nums[1] = 2 + 7 = 9,
+        ///  return [0, 1].
+        ///  回傳 index 位置
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
-        {   
-            int[] nums = { 1, 2, 3 };
-
-            var result = Permute(nums);
-            foreach (var item in result)
-            {
-                Console.WriteLine(string.Join(",", item));
-            }
-            Console.WriteLine();
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            var res = TwoSum(nums, target);
+            Console.WriteLine($"ref: [{res[0]},{res[1]}]");
         }
 
-        public static IList<IList<int>> Permute(int[] nums)
-        {
-            IList<IList<int>> result = new List<IList<int>>();
-            List<int> list = new List<int>();
-            Backtrack(nums, list, result);
-            return result;
-        }
-        
-        private static void Backtrack(int[] nums, List<int> list, IList<IList<int>> result)
-        {
-            if(list.Count == nums.Length)
-            {
-                result.Add(new List<int>(list));
-                return;
-            }
 
-            for(int i = 0; i < nums.Length; i++)
-            {
-                if(list.Contains(nums[i]))
-                {
-                    continue;
-                }
-                list.Add(nums[i]);
-                Backtrack(nums, list, result);
-                list.RemoveAt(list.Count - 1);
-            }
+        /// <summary>
+        /// https://www.itread01.com/content/1543410439.html
+        /// https://ithelp.ithome.com.tw/articles/10217042
+        /// ContainsKey
+        /// https://vimsky.com/zh-tw/examples/usage/c-sharp-dictionary-containskey-method.html
+        /// 
+        /// 使用哈希表，可以将寻找 target - x 的时间复杂度降低到从 O(N)降低到 O(1)。
+        /// 这样我们创建一个哈希表，对于每一个 x，我们首先查询哈希表中是否存在 target - x，然后将 x 插
+        /// 入到哈希表中，即可保证不会让 x 和自己匹配。
+        /// 
+        /// int[] nums 相同 element 只能使用一次
+        /// 透過 Dictionary 去紀錄 元素
+        /// 也可以使用 Hashset 取代
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum(int[] nums, int target)
+        {
+			Dictionary<int, int> dic = new Dictionary<int, int>();
+			for(int i = 0; i < nums.Length; i++)
+			{
+				int left = target - nums[i];
+				if(dic.ContainsKey(left))
+				{
+					return new int[]{dic[left], i};
+				}
+				
+				if(!dic.ContainsKey(nums[i]))
+				{
+					dic[nums[i]] = i;
+				}
+			}
+			
+			return Array.Empty<int>();
         }
     }
 }
--- Console/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ Console/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+806b046917b9534387179101d54b8801cd314540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2694ebb89655ef4629c3e355d69af21dd962e0e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
