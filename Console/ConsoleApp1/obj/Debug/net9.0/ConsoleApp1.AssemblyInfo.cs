994745e480931e54af412d954bb7db93cf512a95
Peter.Chiu <tidub76217@hotmail.com>
2025-03-27 15:59:57 +0800

    測試acction
    測試acction

--- Console/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ Console/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c757a7fe77dc73c54518deb48877213ad2d31e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c791ddf8496392f9b96d047f6ff2a03d085e33c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
